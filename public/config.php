8525f972908b10892cf2067f06345687d8bfffdb
Slonexx <21moriarty21@mail.ru>
2022-07-04 11:57:45 +0600

    Commit

---
+++ public/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Скопируйте данный файл в файл с именем config.php и укажите ваши значения конфигурационных параметров.
+ */
+return [
+    'appId' => 'ac217934-edaf-4975-91ae-a0ea408727de',
+    'appUid' => 'testov.smartinnovations',
+    'secretKey' => 'MfMotwWe278gFKnpspgxLeWyNkr8eAc4upnz9MJtCIyX9N41rOcKkuYJnIR8WHguSSh5kPUzkWbsFoVh62UzamYYIbJHHQGFuGUSTETgVw304VGvDHKB54TtxsRG6l0U',
+    'appBaseUrl' => 'https://marketplace.vetmobile.kz/',
+];
