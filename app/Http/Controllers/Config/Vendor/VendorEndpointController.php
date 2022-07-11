c22260b416439b0206891f6965edc509a368dc4a
Slonexx <21moriarty21@mail.ru>
2022-07-11 18:25:07 +0600

    Check

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Config\Vendor;
 
 use App\Http\Controllers\Controller;
-use AppInstance;
 use Illuminate\Http\Request;
 
 class VendorEndpointController extends Controller
@@ -17,51 +16,8 @@ class VendorEndpointController extends Controller
 
         loginfo("MOYSKLAD => APP", "Received: method=$method, path=$path");
 
-        dd();
+        dd($method);
 
-        $pp = explode('/', $path);
-        $n = count($pp);
-        $appId = $pp[$n - 2];
-        $accountId = $pp[$n - 1];
-
-        loginfo("MOYSKLAD => APP", "Extracted: appId=$appId, accountId=$accountId");
-
-        $app = AppInstance::load($appId, $accountId);
-        $replyStatus = true;
-
-        switch ($method) {
-            case 'PUT':
-                $requestBody = file_get_contents('php://input');
-
-                loginfo("MOYSKLAD => APP", "Request body: " . print_r($requestBody, true));
-
-                $data = json_decode($requestBody);
-
-                $appUid = $data->appUid;
-                $accessToken = $data->access[0]->access_token;
-
-                loginfo("MOYSKLAD => APP", "Received access_token: appUid=$appUid, access_token=$accessToken)");
-
-                if (!$app->getStatusName()) {
-                    $app->accessToken = $accessToken;
-                    $app->status = AppInstance::SETTINGS_REQUIRED;
-                    $app->persist();
-                }
-                break;
-            case 'GET':
-                break;
-            case 'DELETE':
-                $app->delete();
-                $replyStatus = false;
-                break;
-        }
-
-        if (!$app->getStatusName()) {
-            http_response_code(404);
-        } else if ($replyStatus) {
-            header("Content-Type: application/json");
-            echo '{"status": "' . $app->getStatusName() . '"}';
-        }
 
 
 
