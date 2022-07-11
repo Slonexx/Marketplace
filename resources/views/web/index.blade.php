b5902514fcb06912318a81c4c971c0e2ee23fb80
Slonexx <21moriarty21@mail.ru>
2022-07-11 18:26:13 +0600

    Check

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <form action=" {{  route('Check') }} " method="post">
+    <form action=" {{  route('Check') }} " method="get">
         <br>
         <button type="submit" class="btn btn-primary">
             <i class="fa-solid fa-envelope"></i> Чекаю
