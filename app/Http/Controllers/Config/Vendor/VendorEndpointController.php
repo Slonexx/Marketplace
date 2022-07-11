4afbb0563f93c8ed73e9c84163a77e856bc47070
Slonexx <21moriarty21@mail.ru>
2022-07-11 18:31:53 +0600

    Check

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -9,7 +9,7 @@ class VendorEndpointController extends Controller
 {
     public function Activate(){
 
-        require_once 'lib.php';
+       // require_once 'lib.php';
 
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['PATH_INFO'];
