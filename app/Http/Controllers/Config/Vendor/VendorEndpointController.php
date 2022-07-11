315b8d144ab9b74712d514bd0c7f8e149b3373ae
Slonexx <21moriarty21@mail.ru>
2022-07-11 18:19:39 +0600

    Check

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -8,9 +8,11 @@ use Illuminate\Http\Request;
 class VendorEndpointController extends Controller
 {
     public function Activate(){
+
+        require_once 'lib.php';
+
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['PATH_INFO'];
-
 
         loginfo("MOYSKLAD => APP", "Received: method=$method, path=$path");
 
