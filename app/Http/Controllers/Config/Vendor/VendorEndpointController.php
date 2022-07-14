42fca145f50bfd1a88e23934d96ce82c0df0582a
Slonexx <21moriarty21@mail.ru>
2022-07-14 17:48:11 +0600

    new commit

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -9,24 +9,7 @@ use Illuminate\Http\Request;
 class VendorEndpointController extends Controller
 {
     public function Activate(){
-
-        /*require_once 'lib.php';
-
-        $method = $_SERVER['REQUEST_METHOD'];
-        $path = $_SERVER['PATH_INFO'];
-
-        $log = new loginfoController("MOYSKLAD => APP", "Received: method=$method, path=$path");
-
-        $pp = explode('/', $path);
-        $n = count($pp);
-        $appId = $pp[$n - 2];
-        $accountId = $pp[$n - 1];
-
-        $log = new loginfoController("MOYSKLAD => APP", "Extracted: appId=$appId, accountId=$accountId");
-
-        dd($path);*/
-
-        return view('Config.Vendor');
-
+        session_start();
+        dd( $_SESSION["path"]);
     }
 }
