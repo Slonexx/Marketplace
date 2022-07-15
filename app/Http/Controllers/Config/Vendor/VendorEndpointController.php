87009a3670b0e80af9f35e548571d91516ea44c3
Slonexx <21moriarty21@mail.ru>
2022-07-15 16:46:42 +0600

    new commit

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -3,66 +3,41 @@
 namespace App\Http\Controllers\Config\Vendor;
 
 use App\Http\Controllers\Controller;
+use Firebase\JWT\JWT;
 
 class VendorEndpointController extends Controller
 {
     public function Activate(){
 
-        require_once public_path().'/Config'.'/'.'lib.php';
-
-
-        $method = $_SERVER['REQUEST_METHOD'];
-        $path = $_SERVER['PATH_INFO'];
-
-        loginfo("MOYSKLAD => APP", "Received: method=$method, path=$path");
-
-
-        $pp = explode('/', $path);
-        $n = count($pp);
-        $appId = $pp[$n - 2];
-        $accountId = $pp[$n - 1];
-
-        loginfo("MOYSKLAD => APP", "Extracted: appId=$appId, accountId=$accountId");
-
-        $app = AppInstance::load($appId, $accountId);
-        $replyStatus = true;
-
-        switch ($method) {
-            case 'PUT':
-                $requestBody = file_get_contents('php://input');
-
-                loginfo("MOYSKLAD => APP", "Request body: " . print_r($requestBody, true));
-
-                $data = json_decode($requestBody);
-
-                $appUid = $data->appUid;
-                $accessToken = $data->access[0]->access_token;
-
-                loginfo("MOYSKLAD => APP", "Received access_token: appUid=$appUid, access_token=$accessToken)");
-
-                if (!$app->getStatusName()) {
-                    $app->accessToken = $accessToken;
-                    $app->status = AppInstance::SETTINGS_REQUIRED;
-                    $app->persist();
-                }
-                break;
-            case 'GET':
-                break;
-            case 'DELETE':
-                $app->delete();
-                $replyStatus = false;
-                break;
-        }
-
-        if (!$app->getStatusName()) {
-            http_response_code(404);
-        } else if ($replyStatus) {
-            header("Content-Type: application/json");
-            echo '{"status": "' . $app->getStatusName() . '"}';
-        }
+        $this->cfg();
 
     }
 
+    public function cfg(){
+        $cfg = new AppConfig(require(public_path().'/Config/'.'config.php'));
+        dd($cfg);
+    }
 
 
 }
+
+class AppConfig {
+
+    var $appId = 'APP-ID';
+    var $appUid = 'APP-UID';
+    var $secretKey = 'SECRET-KEY';
+
+    var $appBaseUrl = 'APP-BASE-URL';
+
+    var $moyskladVendorApiEndpointUrl = 'https://online.moysklad.ru/api/vendor/1.0';
+    var $moyskladJsonApiEndpointUrl = 'https://online.moysklad.ru/api/remap/1.2';
+
+    public function __construct(array $cfg)
+    {
+        foreach ($cfg as $k => $v) {
+            $this->$k = $v;
+        }
+    }
+}
+
+
--- public/Config/vendor-endpoint.php
+++ public/Config/vendor-endpoint.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'lib.php';
+//require_once 'lib.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'];
