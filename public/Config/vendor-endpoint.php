2fbcd36827c7437d8500d31eeabcbd029bc1c7e5
Slonexx <21moriarty21@mail.ru>
2022-07-15 17:00:56 +0600

    new commit

--- public/Config/vendor-endpoint.php
+++ public/Config/vendor-endpoint.php
@@ -3,19 +3,16 @@
 //require_once 'lib.php';
 
 use App\Http\Controllers\Config\Vendor\AppInstance;
+use App\Http\Controllers\Config\Vendor\log;
 
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'];
-
-app(\App\Http\Controllers\Config\Vendor\log::class)->loginfo("MOYSKLAD => APP", "Received: method=$method, path=$path");
 
 
 $pp = explode('/', $path);
 $n = count($pp);
 $appId = $pp[$n - 2];
 $accountId = $pp[$n - 1];
-
-app(\App\Http\Controllers\Config\Vendor\log::class)->loginfo("MOYSKLAD => APP", "Extracted: appId=$appId, accountId=$accountId");
 
 $app = AppInstance::load($appId, $accountId);
 $replyStatus = true;
@@ -24,14 +21,10 @@ switch ($method) {
     case 'PUT':
         $requestBody = file_get_contents('php://input');
 
-        app(\App\Http\Controllers\Config\Vendor\log::class)->loginfo("MOYSKLAD => APP", "Request body: " . print_r($requestBody, true));
-
         $data = json_decode($requestBody);
 
         $appUid = $data->appUid;
         $accessToken = $data->access[0]->access_token;
-
-        app(\App\Http\Controllers\Config\Vendor\log::class)->loginfo("MOYSKLAD => APP", "Received access_token: appUid=$appUid, access_token=$accessToken)");
 
         if (!$app->getStatusName()) {
             $app->accessToken = $accessToken;
