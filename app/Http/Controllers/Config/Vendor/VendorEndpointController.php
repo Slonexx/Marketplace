d7d7c5adc5029eb9136c2f524eed194dae3dd9be
Slonexx <21moriarty21@mail.ru>
2022-07-15 18:00:27 +0600

    new commit

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -11,11 +11,26 @@ use JetBrains\PhpStorm\Pure;
 class VendorEndpointController extends Controller
 {
     public function Activate(){
-        require(public_path().'/Csonfig/'.'lib.php');
+
+        $contextKey = $_GET['contextKey'];
+
+        $employee = vendorApi()->context($contextKey);
+
+        $uid = $employee->uid;
+        $fio = $employee->shortFio;
+        $accountId = $employee->accountId;
+
+        $isAdmin = $employee->permissions->admin->view;
+
+
+        dd($contextKey);
+
+
+        /*require(public_path().'/Csonfig/'.'lib.php');
         $contextName = 'IFRAME';
         require(public_path().'/Config/'.'user-context-loader.inc.php');
         $app = AppInstance::loadApp($accountId);
-        dd($accountId);
+        dd($accountId);*/
 
        /* require(public_path().'/Config/'.'vendor-endpoint.php');
 
