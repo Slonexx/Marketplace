56bba7b60baa3cf9dca6d7763849aecb0b770c2c
Slonexx <21moriarty21@mail.ru>
2022-07-15 18:53:34 +0600

    new commit

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -10,45 +10,17 @@ use JetBrains\PhpStorm\Pure;
 
 class VendorEndpointController extends Controller
 {
-    public function Activate(){
+    public function Activate()
+    {
 
-        require(public_path().'/Config/'.'lib.php');
+        require(public_path() . '/Config/' . 'lib.php');
         $contextName = 'IFRAME';
-        require(public_path().'/Config/'.'user-context-loader.inc.php');
+        require(public_path() . '/Config/' . 'user-context-loader.inc.php');
         $app = AppInstance::loadApp($accountId);
-        dd($accountId);
-
-        /* require(public_path().'/Config/'.'vendor-endpoint.php');
-
-         $tmp = new \getInfo();
-         $id = $tmp->getAccess_token();
-         dd($id);
 
 
-
-       /*  require(public_path().'/Config/'.'lib.php');
-         $contextKey = $_GET['contextKey'];
-
-         //$employee = vendorApi()->context($contextKey);
+        dd($app);
 
 
-
-         dd($contextKey);*/
-
-
-        /*require(public_path().'/Config/'.'lib.php');
-        $contextName = 'IFRAME';
-        require(public_path().'/Config/'.'user-context-loader.inc.php');
-        $app = AppInstance::loadApp($accountId);
-        dd($accountId);*/
-
-       /* require(public_path().'/Config/'.'vendor-endpoint.php');
-
-        $tmp = new \getInfo();
-        $id = $tmp->getAccess_token();
-        dd($id);*/
     }
-
-
-
 }
