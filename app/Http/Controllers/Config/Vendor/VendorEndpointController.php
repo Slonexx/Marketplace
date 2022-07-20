29259e6c85cbda45a7ddd15e1e75ce3a0fc99f5e
Slonexx <21moriarty21@mail.ru>
2022-07-20 15:50:14 +0600

    new commit

--- app/Http/Controllers/Config/Vendor/VendorEndpointController.php
+++ app/Http/Controllers/Config/Vendor/VendorEndpointController.php
@@ -5,68 +5,12 @@ namespace App\Http\Controllers\Config\Vendor;
 use App\Http\Controllers\Controller;
 use App\Models\personal;
 use App\Http\Controllers\Config\Vendor\AppInstance;
-use Doctrine\Instantiator\Exception\InvalidArgumentException;
-use Firebase\JWT\JWT;
-use Illuminate\Support\Facades\Log;
-use JetBrains\PhpStorm\Pure;
+
 
 class VendorEndpointController extends Controller
 {
     public function Activate()
     {
-
-        $method = $_SERVER['REQUEST_METHOD'];
-        $path = $_SERVER['PATH_INFO'];
-
-
-        $pp = explode('/', $path);
-        $n = count($pp);
-        $appId = $pp[$n - 2];
-        $accountId = $pp[$n - 1];
-
-        personal::firstOrCreate([
-            'path' => $path,],[
-                'path' => $path,
-                'appId' => $appId,
-                'accountId' => $accountId,
-            ]
-
-        );
-
-        $app = AppInstance::load($appId, $accountId);
-        $replyStatus = true;
-
-        switch ($method) {
-            case 'PUT':
-                $requestBody = file_get_contents('php://input');
-
-
-                $data = json_decode($requestBody);
-
-                $appUid = $data->appUid;
-                $accessToken = $data->access[0]->access_token;
-
-
-                if (!$app->getStatusName()) {
-                    $app->accessToken = $accessToken;
-                    $app->status = AppInstance::SETTINGS_REQUIRED;
-                    $app->persist();
-                }
-                break;
-            case 'GET':
-                break;
-            case 'DELETE':
-                $app->delete();
-                $replyStatus = false;
-                break;
-        }
-
-        if (!$app->getStatusName()) {
-            http_response_code(404);
-        } else if ($replyStatus) {
-            header("Content-Type: application/json");
-            echo '{"status": "' . $app->getStatusName() . '"}';
-        }
 
 
     }
