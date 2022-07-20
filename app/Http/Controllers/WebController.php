51a7042c28733cb7885ac9ff76b136b8f65f6798
Slonexx <21moriarty21@mail.ru>
2022-07-20 10:15:58 +0600

    new commit

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -10,10 +10,36 @@ use RealRashid\SweetAlert\Facades\Alert;
 class WebController extends Controller
 {
     public function index(Request $request){
-        dd($request->contextKey);
+
+        $contextKey = $request->contextKey;
+        //dd($request->contextKey);
+        $employee = new VendorApi();
+        dd($employee->context($contextKey));
+
 
         return view('web.index');
     }
 
+
+}
+class VendorApi {
+
+    function context(string $contextKey) {
+        return $this->request('POST', '/context/' . $contextKey);
+    }
+
+    function updateAppStatus(string $appId, string $accountId, string $status) {
+        return $this->request('PUT',
+            "/apps/$appId/$accountId/status",
+            "{\"status\": \"$status\"}");
+    }
+
+    private function request(string $method, $path, $body = null) {
+        return makeHttpRequest(
+            $method,
+            cfg()->moyskladVendorApiEndpointUrl . $path,
+            buildJWT(),
+            $body);
+    }
 
 }
