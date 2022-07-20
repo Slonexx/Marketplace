1092ab23baad60b7b0e8389402a8c663ab2507fd
Slonexx <21moriarty21@mail.ru>
2022-07-20 14:47:52 +0600

    new commit

--- app/Http/Controllers/Config/Lib/AppInstanceContoller.php
+++ app/Http/Controllers/Config/Lib/AppInstanceContoller.php
@@ -75,7 +75,7 @@ class AppInstanceContoller extends Controller
         } else {
             $app = unserialize($data);
         }
-        dd($app->accessToken);
+        dd($app);
         //$_SESSION['currentAppInstance'] = $app;
 
         return $app;
