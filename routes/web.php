b9ea27c72cf96493da417ed0c4c2970b05044a22
Slonexx <21moriarty21@mail.ru>
2022-07-20 16:29:33 +0600

    check VendorEndpointController.php

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\WebController\WhatsappController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [VendorEndpointController::class, 'Activate']);
+//Route::get('/', [VendorEndpointController::class, 'Activate']);
 
-//Route::get('/', [WebController::class, 'index']);
+Route::get('/', [WebController::class, 'index']);
 
 Route::get('/Setting', [Setting_mainController::class, 'index'])->name("Setting_Main");
 Route::post('/SettingSend', [Setting_mainController::class, 'postFormSetting'])->name("Setting_Send");
