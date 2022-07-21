fd8b0e59bd571564a55d783e14ad5ae281abf064
Slonexx <21moriarty21@mail.ru>
2022-07-21 15:45:03 +0600

    new commit

--- app/Http/Controllers/Config/Lib/AppInstanceContoller.php
+++ app/Http/Controllers/Config/Lib/AppInstanceContoller.php
@@ -78,7 +78,7 @@ class AppInstanceContoller extends Controller
 
         $app = (object) $app;
 
-        dd(json_decode($app));
+        dd(($app));
         $_SESSION['currentAppInstance'] = $data;
 
         $AppInstance = new AppInstanceContoller($app->appId, $app->accountId);
