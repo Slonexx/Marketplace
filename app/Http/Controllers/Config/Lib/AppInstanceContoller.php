60b1b858331332ec71bf504915d703a4a732447f
Slonexx <21moriarty21@mail.ru>
2022-07-21 15:42:48 +0600

    new commit

--- app/Http/Controllers/Config/Lib/AppInstanceContoller.php
+++ app/Http/Controllers/Config/Lib/AppInstanceContoller.php
@@ -61,7 +61,7 @@ class AppInstanceContoller extends Controller
 
     private static function buildFilename($appId, $accountId) {
         $dir = public_path().'/Config/';
-        return $dir . "data/$appId.$accountId.json";
+        return $dir . "data/$appId.$accountId.app";
     }
 
     static function loadApp($appId, $accountId): AppInstanceContoller {
