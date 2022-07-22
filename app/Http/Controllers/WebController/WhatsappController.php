716908504525686922a383f4e636dee494e08dab
Slonexx <21moriarty21@mail.ru>
2022-07-22 15:58:58 +0600

    new commit

--- app/Http/Controllers/WebController/WhatsappController.php
+++ app/Http/Controllers/WebController/WhatsappController.php
@@ -10,7 +10,6 @@ class WhatsappController extends Controller
 {
 
     public function Index($accountId){
-       return redirect()->to("https://api.whatsapp.com/send/?phone=87750498821&text&type=phone_number&app_absent=0");
         return view('web.whatsapp', ['accountId' => $accountId] );
     }
 
@@ -23,15 +22,11 @@ class WhatsappController extends Controller
         $name = "Здравствуйте меня зовут ".$request->name.". ";
         $inputName = str_ireplace(" ", "%20", $name);
         $inputMessage = str_ireplace(" ", "%20", $request->message);
-
-        "https://smartkaspi.kz/WhatsappSendNext/".$inputName."/".$inputMessage;
-        return ;
-
-    }
-
-    public function WhatsappSendNext($inputName,$inputMessage ){
         $message = "https://wa.me/87750498821?text=".$inputName.$inputMessage;
         return redirect()->to($message);
+
     }
+
+
 
 }
