3b971023f28b74832c422e9beda91f45042138b3
Slonexx <21moriarty21@mail.ru>
2022-07-22 15:10:21 +0600

    new commit

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::post('/PostSupport/{accountId}', [SupportController::class, 'supportSend'
 
 Route::get('/Whatsapp/{accountId}', [WhatsappController::class, 'Index'])->name("whatsapp");
 Route::post('/WhatsappSend/{accountId}', [WhatsappController::class, 'WhatsappSend'])->name("whatsapp_Send");
-Route::post('/WhatsappSendNext/{inputName}/{inputMessage}', [WhatsappController::class, 'WhatsappSendNext'])->name("WhatsappSendNext");
+Route::get('/WhatsappSendNext/{inputName}/{inputMessage}', [WhatsappController::class, 'WhatsappSendNext'])->name("WhatsappSendNext");
 
