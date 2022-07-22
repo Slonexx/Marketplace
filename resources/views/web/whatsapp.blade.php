0df70643e9d9bc573bdd27ed29a66c7dc109f43b
Slonexx <21moriarty21@mail.ru>
2022-07-22 16:03:51 +0600

    new commit

--- resources/views/web/whatsapp.blade.php
+++ resources/views/web/whatsapp.blade.php
@@ -1,7 +1,7 @@
-@extends('layout')
-
-@section('content')
-
+<!doctype html>
+<html lang="en">
+@include('head')
+<body>
 
     <div class="content p-4 mt-2 bg-white text-black rounded">
         <h2 align="center">Написать в WhatsApp
@@ -23,16 +23,16 @@
 
             @csrf <!-- {{ csrf_field() }} -->
                 <div class="form-group mb-3 row ">
-                    <label for="TokenKaspi" class="col-sm-2 col-form-label mt-2">Введите Имя</label>
-                    <div class="col-sm-10">
+                    <label for="TokenKaspi" class="col-form-label">Введите Имя</label>
+                    <div class="col-sm-12">
                         <input type="text" name="name" placeholder="Введите Имя, фамилия" id="name" class="form-control form-control-orange"
                                required maxlength="100" value="{{ old('name') ?? '' }}">
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <label for="TokenKaspi" class="col-sm-2 col-form-label mt-2"></label>
-                    <div class="col-sm-10">
+                    <label for="TokenKaspi" class="col-form-label"></label>
+                    <div class="col-sm-12">
                         <textarea class="form-control form-control-orange" name="message" placeholder="Ваше сообщение"
                                   required maxlength="500" rows="3">{{ old('message') ?? '' }}</textarea>
                     </div>
@@ -49,7 +49,7 @@
     </div>
 
 
-@endsection
 
 
-
+</body>
+</html>
