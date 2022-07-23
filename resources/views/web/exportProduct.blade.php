417dde09137dfc00f4620b275597f36ba7fa0be4
Slonexx <21moriarty21@mail.ru>
2022-07-23 15:20:46 +0600

    edit support

---
+++ resources/views/web/exportProduct.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('content')
+
+    @php
+
+
+
+        @endphp
+
+
+
+    <div class="content p-4 mt-2 bg-white text-Black rounded">
+        <h1 class=" text-black">  <i class="fas fa-book-open text-orange"></i> Инструкция</h1>
+
+    </div>
+
+
+
+@endsection
+
