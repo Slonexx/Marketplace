188cbd52e4ac82e728586abc152b397af8e86c78
Slonexx <21moriarty21@mail.ru>
2022-07-23 10:26:39 +0600

    check a href

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,10 @@
                         <a  target="_blank" href={{  route('whatsapp', ['accountId' => $accountId] ) }} >
                             <i class="fa-brands fa-whatsapp"></i>
                             Написать в WhatsApp </a>
+
+                        <a  target="_blank" href="https://l.clck.bar/f5334">
+                            <i class="fa-brands fa-whatsapp"></i>
+                            Написать в WhatsApp + </a>
                     </div>
             </div>
 
