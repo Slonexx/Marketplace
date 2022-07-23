2c8a194a490036de5470f77235879c3a1f802242
Slonexx <21moriarty21@mail.ru>
2022-07-23 09:59:29 +0600

    edit config

--- public/Config/config.php
+++ public/Config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'appId' => 'ac217934-edaf-4975-91ae-a0ea408727de',
-    'appUid' => 'testov.smartinnovations',
+    'appUid' => 'kaspi.smartinnovations',
     'secretKey' => 'MfMotwWe278gFKnpspgxLeWyNkr8eAc4upnz9MJtCIyX9N41rOcKkuYJnIR8WHguSSh5kPUzkWbsFoVh62UzamYYIbJHHQGFuGUSTETgVw304VGvDHKB54TtxsRG6l0U',
     'appBaseUrl' => 'https://smartkaspi.kz/',
 ];
