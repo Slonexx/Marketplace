25ef934c78deb23807a753cc5d1c6e7aecb806e8
Slonexx <21moriarty21@mail.ru>
2022-07-23 22:12:18 +0600

    new Commit

---
+++ app/Http/Controllers/WebController/InfoLogController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\webController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InfoLogController extends Controller
+{
+    //
+}
