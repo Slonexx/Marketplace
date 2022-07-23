b0146d6415d4899745efdddc34818bc0aec87a3c
1ntellekt <pitetvasa@gmail.com>
2022-07-23 16:23:26 +0600

    export product controller

--- app/Http/Controllers/WebController/ExportProductController.php
+++ app/Http/Controllers/WebController/ExportProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\WebController;
 
+use App\Http\Controllers\ApiClientMC;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,4 +11,25 @@ class ExportProductController extends Controller
     public function index($accountId){
         return view('web.exportProduct', ['accountId' => $accountId]);
     }
+
+    public function getProductCount($apiKey)
+    {
+        $uri = "https://online.moysklad.ru/api/remap/1.2/entity/product";
+        $client = new ApiClientMC($uri,$apiKey);
+        $jsonProducts = $client->requestGet();
+        $count = 0;
+        foreach($jsonProducts->rows as $row){
+            if(property_exists($row, 'attributes')){
+                 foreach($row->attributes as $attrib){
+                    if( $attrib->name == 'Добавлять товар на Kaspi' 
+                        && $attrib->type == 'boolean' && $attrib->value == 1)
+                    {
+                        $count++;
+                    }
+                }
+            }
+        }
+        return $count;
+    }
+
 }
