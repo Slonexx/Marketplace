2f1ccd1c948874335f9bdb8c14ebc830e9da0be7
Slonexx <21moriarty21@mail.ru>
2022-07-23 08:36:26 +0600

    new commit

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -8,12 +8,19 @@
 
     @endphp
 
+
+
     <div class="content p-4 mt-2 bg-white text-Black rounded">
-        <form action=" {{ route('Check', ['accountId' => $accountId] ) }} " method="get">
+
+        <div class="embed-responsive embed-responsive-16by9 ">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/iBlyGEGOPcI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        {{--<form action=" {{ route('Check', ['accountId' => $accountId] ) }} " method="get">
             <br>
             <button type="submit" class="btn btn-primary"><i class="fa-solid fa-envelope"></i> SERGEI CHECK </button>
-        </form>
-    <div
+        </form>--}}
+    <div>
 
 
 @endsection
