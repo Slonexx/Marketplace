18a24ad1790923cb322e58eefd2696b21b033224
Slonexx <21moriarty21@mail.ru>
2022-07-29 09:38:53 +0600

    fix text

--- resources/views/web/exportProduct.blade.php
+++ resources/views/web/exportProduct.blade.php
@@ -19,7 +19,7 @@
 
 
             </div>
-            <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+           {{-- <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -34,7 +34,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>--}}
 
         </div>
 
