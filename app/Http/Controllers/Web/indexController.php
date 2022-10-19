f7c6b3b62afc706520fa501103cd181fbcc2be0c
Slonexxx <21moriarty21@mail.ru>
2022-10-19 11:37:51 +0600

    update marketplace 2.0 new BD, observer, logic, route list, controller and new design

--- app/Http/Controllers/Web/indexController.php
+++ app/Http/Controllers/Web/indexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Clients\MsClient;
 use App\Http\Controllers\Config\Lib\VendorApiController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\getData\getSetting;
 use Illuminate\Http\Request;
 
 class indexController extends Controller
@@ -42,4 +44,38 @@ class indexController extends Controller
             'isAdmin' => $isAdmin,
         ] );
     }
+
+    public function widgetInfoAttributes(Request $request){
+        $ticket_id = null;
+
+        $accountId = $request->accountId;
+        $entity_type = $request->entity_type;
+        $objectId = $request->objectId;
+
+        $url = $this->getUrlEntity($entity_type, $objectId);
+        $Setting = new getSetting($accountId);
+        $Client = new MsClient($Setting->tokenMs);
+        $body = $Client->get($url);
+
+        if (property_exists($body, 'attributes')){
+            foreach ($body->attributes as $item){
+                if ($item->name == 'id-билета (ReKassa)'){
+                    if ($item->value != null) $ticket_id = $item->value;
+                    break;
+                }
+            }
+        }
+        return response()->json(['ticket_id' => $ticket_id]);
+    }
+
+    private function getUrlEntity($enType,$enId){
+        return match ($enType) {
+            "customerorder" => "https://online.moysklad.ru/api/remap/1.2/entity/customerorder/" . $enId,
+            "demand" => "https://online.moysklad.ru/api/remap/1.2/entity/demand/" . $enId,
+            "salesreturn" => "https://online.moysklad.ru/api/remap/1.2/entity/salesreturn/" . $enId,
+            default => null,
+        };
+    }
+
+
 }
