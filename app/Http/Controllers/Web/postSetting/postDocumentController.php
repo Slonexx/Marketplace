fc1b926ff2a0942e7bd78032a63c68b791389969
Slonexxx <21moriarty21@mail.ru>
2022-10-20 17:02:29 +0600

    update marketplace 2.0 new BD, observer, logic, route list, controller and new design

--- app/Http/Controllers/Web/postSetting/postDocumentController.php
+++ app/Http/Controllers/Web/postSetting/postDocumentController.php
@@ -10,21 +10,31 @@ use Illuminate\Http\Request;
 
 class postDocumentController extends Controller
 {
-    public function postDocument(Request $request, $accountId): \Illuminate\Http\RedirectResponse
+    public function postDocument(Request $request, $accountId): \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
     {
         $isAdmin = $request->isAdmin;
         $apiKey = 'f5ac6559-b5cd-4e0e-89e5-7fd32a6d60a5';
         $app = new getSetting($accountId);
+
         try {
             DataBaseService::updateSetting($accountId, $app->tokenMs, $apiKey,
                 $request->paymentDocument,null,null);
+            $message = [
+                'alert' => ' alert alert-success alert-dismissible fade show in text-center ',
+                'message' => ' Настройки сохранились ',
+            ];
         } catch (\Throwable $e){
-
+            $message = [
+                'alert' => ' alert alert-danger alert-dismissible fade show in text-center ',
+                'message' => $e->getMessage(),
+            ];
         }
 
-        return redirect()->route('getWorker', [
+        return view('setting.document', [
             'accountId' => $accountId,
             'isAdmin' => $isAdmin,
+            'message' => $message,
+            'paymentDocument' =>  $request->paymentDocument,
         ]);
     }
 }
--- app/Http/Controllers/Widget/customerorder/customerorderEditController.php
+++ app/Http/Controllers/Widget/customerorder/customerorderEditController.php
@@ -5,18 +5,30 @@ namespace App\Http\Controllers\Widget\customerorder;
 use App\Http\Controllers\Config\Lib\cfg;
 use App\Http\Controllers\Config\Lib\VendorApiController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\getData\getDeviceFirst;
+use App\Http\Controllers\getData\getDevices;
+use App\Http\Controllers\getData\getMainSetting;
+use App\Http\Controllers\getData\getSetting;
 use App\Http\Controllers\getData\getWorkerID;
 use App\Http\Controllers\getData\getWorkers;
+use App\Models\mainSetting;
 use Illuminate\Http\Request;
 
 class customerorderEditController extends Controller
 {
     public function customerorder(Request $request){
 
-        $contextKey = $request->contextKey;
+        //$contextKey = $request->contextKey;
         $vendorAPI = new VendorApiController();
-        $employee = $vendorAPI->context($contextKey);
-        $accountId = $employee->accountId;
+        //$employee = $vendorAPI->context($contextKey);
+        //$accountId = $employee->accountId;
+        $accountId = "1dd5bd55-d141-11ec-0a80-055600047495";
+
+        $Devices = new getDevices($accountId);
+        if ($Devices->devices) $tmp = "yes";
+        else $tmp = "no";
+        dd($tmp);
+
 
         $entity = 'counterparty';
 
