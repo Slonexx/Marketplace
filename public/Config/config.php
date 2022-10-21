a5ed76e137fc56059ca04b1bb0e9efe06a49b8e2
Slonexxx <21moriarty21@mail.ru>
2022-10-21 17:15:12 +0600

    update marketplace 2.0 new BD, observer, logic, route list, controller and new design

--- public/Config/config.php
+++ public/Config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'appId' => 'fc1fdbc6-a30f-4aba-8fab-15dc02080b8e',
-    'appUid' => 'dev.smartinnovations',
-    'secretKey' => 'Q7aSgtXX3JJMvkxzM5No3qMnWUZXnxuYz4x81vtfvrKAepe3FOT1gRFmri6jiTQRsIBbBGFu1dQr6pnpa1VjKbyGoce4Wv7Qz3vLFF7Y564p7nUwntqs7ik7UV8x0GHp',
-    'appBaseUrl' => 'https://dev.smartkaspi.kz/',
+    'appId' => 'ac217934-edaf-4975-91ae-a0ea408727de',
+    'appUid' => 'kaspi.smartinnovations',
+    'secretKey' => 'MfMotwWe278gFKnpspgxLeWyNkr8eAc4upnz9MJtCIyX9N41rOcKkuYJnIR8WHguSSh5kPUzkWbsFoVh62UzamYYIbJHHQGFuGUSTETgVw304VGvDHKB54TtxsRG6l0U',
+    'appBaseUrl' => 'https://smartkaspi.kz/',
 ];
