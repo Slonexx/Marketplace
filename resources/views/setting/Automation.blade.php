76e60e997fe3fbfce68d56b05f1c9ed7b9af659a
Slonexxx <21moriarty21@mail.ru>
2023-10-24 16:22:29 +0600

    new api adress ms

--- resources/views/setting/Automation.blade.php
+++ resources/views/setting/Automation.blade.php
@@ -8,11 +8,8 @@
         <div class="row rekassa_gradient rounded p-2">
             <div class="col-10">
                 <div class="mx-2"> <img src="https://app.rekassa.kz/static/logo.png" width="35" height="35"  alt="">
-                    <span class="text-white"> Настройки &#8594; ReKassa &#8594; кассовый аппарат </span>
+                    <span class="text-white"> Настройки &#8594; ReKassa &#8594; Автоматизация </span>
                 </div>
-            </div>
-            <div class="col-2">
-                <button onclick="add_device()" type="button" class=" btn transparent btn-outline-warning"> <i class="fa-solid fa-circle-plus"></i> Добавить </button>
             </div>
         </div>
 
